220fce25e36028cbafb87d2c9745f5ba055b5f4c
kitakoj18 <kitakoj18@gmail.com>
2020-06-17 17:38:01 -0700

    connected feed to backend

--- screens/Feed.js
+++ screens/Feed.js
@@ -1,11 +1,45 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, FlatList, StyleSheet } from 'react-native';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
+
+import axios from 'axios';
 
 import Post from '../components/FeedPosts/Post';
 import AddButton from '../components/FeedPosts/AddPost/AddButton';
 
 const Feed = props =>{
+
+    const [uploadedPosts, setUploadedPosts] = useState();
+
+    useEffect(() =>{
+
+        axios.get('http://localhost:8080/posts/get')
+            .then(response =>{
+                const retrievedPosts = response.data.posts;
+                setUploadedPosts(retrievedPosts);
+            })
+
+    }, [])
+
+    const renderPosts = (result) =>{
+
+        const recipeTitle = result.item.recipeTitle;
+        const description = result.item.description;
+        const imgUri = 'http://localhost:8080/' + result.item.imageUrl;
+        const prepTime = result.item.prepTime.toString();
+        const cookTime = result.item.cookTime.toString();
+
+        return(
+            <Post 
+                onSelectDetailHandler={onSelectDetailHandler}
+                recipeTitle={recipeTitle}
+                imgUri={imgUri}
+                description={description}
+                prepTime={prepTime}
+                cookTime={cookTime}
+            />
+        )
+    }
 
     const onSelectDetailHandler = () => {
         props.navigation.navigate({
@@ -21,9 +55,15 @@ const Feed = props =>{
         //     <Text>This is going to be the feed!</Text>
         // </View>
         <View style={styles.screen}>
-            <Post 
-                onSelectDetailHandler={onSelectDetailHandler}
+            <FlatList
+                data={uploadedPosts}
+                keyExtractor={result => result._id}
+                renderItem={renderPosts}
+                showsVerticalScrollIndicator={false}
             />
+            {/* <Post 
+                onSelectDetailHandler={onSelectDetailHandler}
+            /> */}
         </View>
     )
 }
