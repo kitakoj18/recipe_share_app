fa786bf38fec2dba119853bea529fdc86ee50b8c
kitakoj18 <kitakoj18@gmail.com>
2020-06-26 14:32:57 -0700

    added async dispatch to perform login action

--- store/actions/auth.js
+++ store/actions/auth.js
@@ -1,5 +1,27 @@
+import axios from 'axios'; 
+
 export const LOGIN = 'LOGIN';
 
 export const login = (email, password) =>{
 
+    const authRoute = 'http://localhost:8080/auth/login';
+    const body = {
+        email: email,
+        password: password
+    }
+
+    return async dispatch => {
+        const res = await axios.post(authRoute, body)
+        if(res.errorMsg){
+            //send alert with error message to user ie if email or password is incorrect
+            throw new Error(res.errorMsg);
+        }
+
+        const resData = res.data;
+        dispatch({
+            type: LOGIN,
+            token: resData.token,
+            userId: resData.userId
+        })
+    };
 };
